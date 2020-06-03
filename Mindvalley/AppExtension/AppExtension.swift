8b6841e308b4c8b366a44a46c74f7aa7dd2fcefb
Maniganda Saravanan <manigandadp@gmail.com>
2020-06-03 10:50:08 +0400

    changes

---
+++ Mindvalley/AppExtension/AppExtension.swift
@@ -0,0 +1,13 @@
+//
+//  AppExtension.swift
+//  Mindvalley
+//
+//  Created by Maniganda Saravanan on 03/06/2020.
+//  Copyright © 2020 Maniganda Saravanan. All rights reserved.
+//
+
+import UIKit
+
+class AppExtension: NSObject {
+
+}
---
+++ Mindvalley/AppUtilities/AppUtilities.swift
@@ -0,0 +1,13 @@
+//
+//  AppUtilities.swift
+//  Mindvalley
+//
+//  Created by Maniganda Saravanan on 03/06/2020.
+//  Copyright © 2020 Maniganda Saravanan. All rights reserved.
+//
+
+import UIKit
+
+class AppUtilities: NSObject {
+
+}
